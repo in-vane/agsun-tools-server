c51a191b4f0ff8d8c31238092b1e295079d3375d
guzijie <1661873784@qq.com>
2024-05-05 12:28:45 +0800

    feat: add check_line.py

--- handlers/check_line.py
+++ handlers/check_line.py
@@ -11,22 +11,23 @@ CODE_ERROR = 1
 
 def thicken_lines_in_all_pages(doc, new_line_width=0.5):
     for page_number in range(len(doc)):
-        if page_number == 6:
-            page = doc[page_number]
-            # Initialize a new shape object on the page to draw the modified lines
-            shape = page.new_shape()
-            # Analyze the page for vector drawings
-            for item in page.get_drawings():
-                if item['type'] == 's' and item['width'] < 0.08 and item['stroke_opacity'] == 1.0 and item[
-                    'dashes'] == "[] 0":
-                    for subitem in item['items']:
-                        if subitem[0] == 'l':  # Check for line items
-                            start_point, end_point = subitem[1], subitem[2]
-                            new_color = (1, 0, 0)  # Red color
-                            shape.draw_line(start_point, end_point)
-                            shape.finish(width=new_line_width, color=new_color, stroke_opacity=item['stroke_opacity'])
-            # Commit the drawing operations to the page
-            shape.commit()
+        page = doc[page_number]
+        # Initialize a new shape object on the page to draw the modified lines
+        shape = page.new_shape()
+        # Analyze the page for vector drawings
+        for item in page.get_drawings():
+            if item['type'] == 's' and item['width'] < 0.08 and item['stroke_opacity'] == 1.0 and item[
+                'dashes'] == "[] 0":
+                for subitem in item['items']:
+                    if subitem[0] == 'l':  # Check for line items
+                        start_point, end_point = subitem[1], subitem[2]
+                        new_color = (1, 0, 0)  # Red color
+                        shape.draw_line(start_point, end_point)
+                        shape.finish(width=new_line_width, color=new_color, stroke_opacity=item['stroke_opacity'])
+        # Commit the drawing operations to the page
+        shape.commit()
+
+
 
     # doc.save(PDF_OUTPUT)
 
